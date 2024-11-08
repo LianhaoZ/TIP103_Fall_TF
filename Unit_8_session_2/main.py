5d349d6480724189aa0e745c1c87deaaa4dda788
Lianhao Zheng <lianhaoz06@gmail.com>
2024-11-07 18:12:12 -0600

    Added Unit_8_session_2

--- Unit_8_session_2/main.py
+++ Unit_8_session_2/main.py
@@ -1,90 +1,74 @@
 # PROBLEM 1
 # Understand
 # Share 2 questions you would ask to help understand the question:
-# - can I use a dijkstra algorithm?
+# - can I use the sorting function?
 # - are there any time constraints? 
 
 # Plan
 # Write out in plain English what you want to do: 
-# I first construct the graph using a adjacency matrix, keeping weight as the value of 2 nodes. Then I compute the longest time to complete the shortest
-# path to any node. This is done via dijkstra and taking the maximum of resulting distance from source node k.
+# I will create a sorting function that given 2 numbers A, B, greedily compute if combining AB or BA will give the bigger number and order than as such. 
+# Then using the sorting function I can sort the elements in num and return the sorted answer at the end.
 
 # Implement
 # Translate the pseudocode into Python and share your final answer: 
-import heapq
-def networkDelayTime(times, n: int, k: int) -> int:
-    g = [[-1]*n for _ in range(n)] 
-    for x , y , w in times:
-        g[x-1][y-1] = w
+from functools import cmp_to_key
+def largestNumber(nums):
+    def compare(a, b): 
+        ab = str(a) + str(b)   
+        ba = str(b) + str(a)  
+        if ab > ba: #a before b 
+            return -1 
+        elif ab < ba:  #b before a
+            return 1
+        else: #b equal a
+            return 0
 
+    res = sorted([str(i) for i in nums], key = cmp_to_key(compare))
 
-    dist = [float('inf')] * n
-    def dijkstra(src):
-        pq = [(0, src)] 
-        dist[src] = 0
-        while pq:
-            current_dist, u = heapq.heappop(pq)
-
-            for v in range(n):  
-                if g[u][v] != -1:
-                    weight = g[u][v] 
-                    if dist[v] > current_dist + weight: 
-                        dist[v] = current_dist + weight
-                        heapq.heappush(pq, (dist[v], v))
-
-    dijkstra(k - 1) 
-    res = 0
-    for i in dist:
-        if i == float('inf'):
-            return -1
-        res = max(res, i)
-    return res
-            
+    return str(int(''.join(res)))
 
             
 # Review / Evaulate 
-# n is number of total nodes, e is total number of edges.
-# O((n*n) log n) time performing dijkstra with priority queue for worst case dense graph, average O((n+e)log n)
-# O(n * n) space for storing all possible nodes in an adj matrix
+# n is length of numbers
+# O(n log n) time for sorting 
+# O(n) space for sorting in python and storing the result array
 
 
 
 # PROBLEM 2
 # Understand
 # Share 2 questions you would ask to help understand the question:
-# - what if the graph is empty?
+# - what if there are no answers?
 # - Are there any additional time constraints?
 
 # Plan
 # Write out in plain English what you want to do: 
-# For each newly added edge, I will add the edge to the adjacency list and then dfs to check if there is a connect component/cycle. 
-# If so then I return the added edge.
+# answers[i] equals to total number of rabbits seen besides current rabbit, increment result counter by (answers[i] + 1) for potential future rabbits seen. 
+# everytime this current number of rabbits has been seen, decrement the current counter for answers[i]. If this count reaches 0, delete from dict. 
+# Idea: A color recorded can only be recorded answers[i] times -> rabbit can say same number answers[i] times. 
+# keep track of this number and add to result (answers[i] + 1) everytime when answers[i] is encountered more than answers[i] amount of times.
+
 
 # Implement
 # Translate the pseudocode into Python and share your final answer: 
-def findRedundantConnection(edges):
-    def dfs(graph, node, seen, prev): 
-        if node in seen:
-            return False
-        seen.add(node)
-        check = True
-        for i in graph[node]: 
-            if i == prev:
-                continue
-            if not dfs(graph, i, seen, node):
-                check = False
-        return check
-        
-    g = {i:[] for i in range(1000)}
-    for prereq in edges: 
-        g[prereq[0]-1].append(prereq[1]-1) 
-        g[prereq[1]-1].append(prereq[0]-1) 
-        seen = set()
-        if not dfs(g, prereq[0]-1, seen, None):
-            return prereq   
- 
+def numRabbits(answers) -> int:
+    seen = {}
+    cnt = 0
+    for i in answers: 
+        if i == 0:
+            cnt += 1
+            continue
+        if i not in seen:
+            seen[i] = [i, i]
+            cnt += (i + 1) 
+        else: 
+            seen[i][1] -= 1
+            if seen[i][1] == 0:
+                del seen[i] 
+    return cnt
+                  
 
 # Review / Evaulate
-# n is number of total nodes (max will be 1000, so set to 1000), e is total number of edges.
-# O(e * (n + e)) -> O(e * (1000 + e)) -> O(e * (e)) need to check all nodes with dfs with each added edge  
-# O(n+e) -> O(e) space for storing all edges for each node in adj list, n is constant at 1000
+# n is number of answers
+# O(n) b/c looping through answers array, each operation within the loop is O(1)
+# O(n) because storing previous seen answers worst case stores every answer. 
